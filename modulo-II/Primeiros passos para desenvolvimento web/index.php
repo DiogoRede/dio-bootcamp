d1cc106a5c5711ef48d38fc98846081635aa366f
DiogoRede <diogorede2021@gmail.com>
2022-07-04 20:02:20 -0300

    Inserir Curso - Introdução a criação de websites com HTML5 e CSS3

--- modulo-II/Primeiros passos para desenvolvimento web/index.php
+++ modulo-II/Primeiros passos para desenvolvimento web/index.php
@@ -1,51 +1,51 @@
 <!doctype html>
 <html lang="pt-br">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
-  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
-  <title>Desafio - accordion</title>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link
+            rel="stylesheet"
+            href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+        <title>Desafio - accordion</title>
 
-  <script>
-  $( function() {
-    $( "#accordion" ).accordion();
-  } );
-  </script>
+        <script>
+            $(function () {
+                $("#accordion").accordion();
+            });
+        </script>
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-<div id="accordion">
+        <div id="accordion">
 
-  <h3>1</h3>
-  <div>
-    <p>
-    1 accordion
-    </p>
-  </div>
+            <h3>1</h3>
+            <div>
+                <p>
+                    1 accordion
+                </p>
+            </div>
 
-  <h3>2</h3>
-  <div>
-    <p>2 accordion</p>
-  </div>
+            <h3>2</h3>
+            <div>
+                <p>2 accordion</p>
+            </div>
 
-  <h3>3</h3> 
-  <div>
-    <p>3 accordion</p>
-  </div>
+            <h3>3</h3>
+            <div>
+                <p>3 accordion</p>
+            </div>
 
-  <h3>Section 4</h3>
-  <div>
-    <p>4 accordion</p>
-  </div>
+            <h3>Section 4</h3>
+            <div>
+                <p>4 accordion</p>
+            </div>
 
-
-</div>
-
+        </div>
 
   <?php
 
@@ -54,5 +54,5 @@
 
   ?>
 
-</body>
+    </body>
 </html>
