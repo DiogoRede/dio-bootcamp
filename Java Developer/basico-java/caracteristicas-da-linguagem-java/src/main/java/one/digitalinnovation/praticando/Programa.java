7d05f27fa6d2af19ec2929b432d66c17eaa28b77
DiogoRede <diogorede2021@gmail.com>
2022-07-14 16:10:53 -0300

    Inserindo arquivos da aula

---
+++ Java Developer/basico-java/caracteristicas-da-linguagem-java/src/main/java/one/digitalinnovation/praticando/Programa.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.praticando;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        String nome = "Diogo";
+        String sobrenome = "Rede";
+
+        System.out.println(nome.concat(sobrenome));
+
+        
+
+    }
+
+}
